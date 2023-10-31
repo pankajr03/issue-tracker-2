3d6a2f531e6410cdf7d246c604cc3212220b25d7
pankajr03 <pankaj1211ranout@gmail.com>
2023-10-31 18:28:09 +0530

    Handle form submission with react-hook-form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,19 +3,38 @@ import { TextField, TextArea, Button } from '@radix-ui/themes'
 import React from 'react'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import {useForm, Controller} from "react-hook-form"
+import { Issue } from '@prisma/client';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 
+interface IssueForm {
+    title: string;
+    description: string
+}
 const NewIssue = () => {
+    const {register, control, handleSubmit} = useForm<IssueForm>()
+    const router = useRouter()
   return (
-    <div className='max-w-xl space-y-4 p-6'>
+    <form className='max-w-xl space-y-4 p-6' onSubmit={handleSubmit(
+        async(data) => {
+            await axios.post('/api/issues',data);
+            router.push('/')
+        
+        })} >
         <TextField.Root>
-        <TextField.Input placeholder="Enter title" />
+        <TextField.Input placeholder="Enter title" {...register('title')}/>
         </TextField.Root>
         
-        <SimpleMDE placeholder="Enter comment…" />
-
+        <Controller 
+        name="description" 
+        control={control} 
+        render={({field})=> <SimpleMDE placeholder="Enter comment…" {...field} />}
+        />
+        
         <Button>Add Issue</Button>
 
-    </div>
+    </form>
   )
     
 }
