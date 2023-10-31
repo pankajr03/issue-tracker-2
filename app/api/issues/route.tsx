43a8ed299a20673e99a41636d7bc69886cddcb2f
pankajr03 <pankaj1211ranout@gmail.com>
2023-10-31 18:50:22 +0530

    Handle errors

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod"
 
 const schema = z.object({
-    title: z.string().min(1).max(255),
+    title: z.string().min(1, 'Title is requried and it should have min one character').max(255),
     description: z.string().min(5)
 })
 
@@ -11,7 +11,7 @@ export async function POST(request: NextRequest) {
     const body = await request.json()
     const validation = schema.safeParse(body)
     if (!validation.success) 
-        return NextResponse.json(validation.error.errors, {status: 400})
+        return NextResponse.json(validation.error.format(), {status: 400})
     
     const newIssue = await prisma.issue.create({
         data: {
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,12 +1,13 @@
 'use client'
-import { TextField, TextArea, Button } from '@radix-ui/themes'
-import React from 'react'
+import { TextField, Button, Callout } from '@radix-ui/themes'
+import React, { useState } from 'react'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import {useForm, Controller} from "react-hook-form"
 import { Issue } from '@prisma/client';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
+import { AiFillInfoCircle } from 'react-icons/ai'
 
 interface IssueForm {
     title: string;
@@ -15,26 +16,45 @@ interface IssueForm {
 const NewIssue = () => {
     const {register, control, handleSubmit} = useForm<IssueForm>()
     const router = useRouter()
+    const [error, setError] = useState('')
   return (
-    <form className='max-w-xl space-y-4 p-6' onSubmit={handleSubmit(
-        async(data) => {
-            await axios.post('/api/issues',data);
-            router.push('/')
-        
-        })} >
-        <TextField.Root>
-        <TextField.Input placeholder="Enter title" {...register('title')}/>
-        </TextField.Root>
-        
-        <Controller 
-        name="description" 
-        control={control} 
-        render={({field})=> <SimpleMDE placeholder="Enter comment…" {...field} />}
-        />
-        
-        <Button>Add Issue</Button>
+    <div className='max-w-xl p-6 mb-6 space-y-4 '>
+        {error && 
+        <Callout.Root>
+            <Callout.Icon>
+                <AiFillInfoCircle />
+            </Callout.Icon>
+            <Callout.Text>
+                {error}
+            </Callout.Text>
+            </Callout.Root>
+}
+        <form className='space-y-4 ' onSubmit={handleSubmit(
+            async(data) => {
+                
+                try {
+                    await axios.post('/api/issues',data);
+                    router.push('/')    
+                } catch (error) {
+                    setError('It is an unexpected error!')
+                }
+                
+            
+            })} >
+            <TextField.Root>
+            <TextField.Input placeholder="Enter title" {...register('title')}/>
+            </TextField.Root>
+            
+            <Controller 
+            name="description" 
+            control={control} 
+            render={({field})=> <SimpleMDE placeholder="Enter comment…" {...field} />}
+            />
+            
+            <Button>Add Issue</Button>
 
-    </form>
+        </form>
+    </div>
   )
     
 }
