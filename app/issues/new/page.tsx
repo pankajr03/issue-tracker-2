f9040f6957969506192149b545b2138e3610a3a9
pankajr03 <pankaj1211ranout@gmail.com>
2023-10-31 16:47:38 +0530

    Added simplemde editor to textarea field description

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { TextField, TextArea, Button } from '@radix-ui/themes'
 import React from 'react'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssue = () => {
   return (
@@ -9,7 +11,7 @@ const NewIssue = () => {
         <TextField.Input placeholder="Enter title" />
         </TextField.Root>
         
-        <TextArea placeholder="Enter comment…" />
+        <SimpleMDE placeholder="Enter comment…" />
 
         <Button>Add Issue</Button>
 
