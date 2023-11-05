38e7a4e8ced81a4fe7e5aad0e2fb2a87d34a8641
pankajr03 <pankaj1211ranout@gmail.com>
2023-11-05 11:21:52 +0530

    Delete issue api ready to use

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -29,3 +29,21 @@ export async function PATCH( request: NextRequest, {params}: {params: {id: strin
     })
     return NextResponse.json(updateIssue, {status: 201})
 }
+
+export async function DELETE( request: NextRequest, {params}: {params: {id: string}}) {
+    
+    const issue = await prisma.issue.findUnique({
+        where: {
+            id: parseInt(params.id)
+        }
+    })
+    if (!issue) 
+        return NextResponse.json({ error: 'Invalid issue'}, {status: 404})
+    
+    const DeleteIssue = await prisma.issue.delete({
+        where: {
+            id: parseInt(params.id)
+        }
+    })
+    return NextResponse.json({}, {status: 201})
+}
