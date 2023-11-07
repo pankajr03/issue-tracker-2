aefe2e2d8e93969b7513c8887c02d700d8ff48c4
pankajr03 <pankaj1211ranout@gmail.com>
2023-11-07 12:03:42 +0530

    Fetching data with useQuery and handling isLoading and error too

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,19 +1,20 @@
 'use client'
 import { User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
+import { QueryClient, useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import Skeleton from '@/app/components/Skeleton'
 
 const AssigneeSelect = () => {
-  
-  const [users, setUsers] = useState<User[]>([])
-  useEffect(()=>{
-    const fetchUser = async() => {
-      const {data} = await axios.get<User[]>('/api/users')
-      setUsers(data)
-    }
-    fetchUser()
-  },[])
+  const {data: users, error, isLoading} = useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then(res => res.data),
+    staleTime: 60 * 1000,
+    retry: 2
+  })
+
+  if (isLoading) return <Skeleton width={"15rem"} />
   
   
   return (
@@ -22,7 +23,7 @@ const AssigneeSelect = () => {
         <Select.Content>
             <Select.Group>
                 <Select.Label>Suggestions</Select.Label>
-                {users.map((user)=> <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item> )}
+                {users?.map((user)=> <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item> )}
                 
             </Select.Group>
             
