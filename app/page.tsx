bb92e7c2c997e738b8c37193fa0e5d5e800a802a
pankajr03 <pankaj1211ranout@gmail.com>
2023-11-09 15:54:24 +0530

    Restucture the dashboard page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Flex, Text, Button } from '@radix-ui/themes'
+import { Flex, Text, Button, Grid } from '@radix-ui/themes'
 import { useRouter } from 'next/navigation'
 import Pagination from './Pagination'
 import LatestIssues from './LatestIssues'
@@ -23,10 +23,14 @@ export default async function Home({searchParams}:  {searchParams: {page: string
   
 
   return (
-    <div>
+    <Grid columns={{initial: "1",md: "2"}} gap={"5"}>
+      <Flex direction={'column'} gap="5">
       <IssueSummary open={countOpen} closed={countClosed} in_progress={countInProgress}/>
       <IssueChart open={countOpen} closed={countClosed} in_progress={countInProgress}/>
+      
+      </Flex>
       <LatestIssues />
-    </div>
+    </Grid>
+   
   )
 }
